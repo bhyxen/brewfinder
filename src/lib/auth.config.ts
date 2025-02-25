6f61f7a9a257c87c4bfb796c45ecc3f49cdd09d3
Brayan Monge Méndez <bmongemendez@gmail.com>
2025-02-25 00:42:29 -0600

    feat: update auth and new Google provider

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,4 +1,5 @@
 import GitHub, { GitHubProfile } from "next-auth/providers/github";
+import Google, { GoogleProfile } from "next-auth/providers/google";
 import { NextAuthConfig } from "next-auth";
 
 export const openRoutes = [
@@ -26,6 +27,22 @@ export default {
 					name: profile.name,
 					email: profile.email,
 					image: profile.avatar_url,
+				};
+			},
+		}),
+		Google({
+			profile: (profile: GoogleProfile) => {
+				return {
+					// Ensure the 'id' (sub) is returned to fix OAuthAccountNotLinked error
+					// https://github.com/nextauthjs/next-auth/issues/9992#issuecomment-2585799270
+					id: profile.sub,
+					firstName: profile.given_name,
+					lastName: profile.family_name,
+					email: profile.email,
+					image: profile.picture,
+					username:
+						`${profile.given_name}${profile.family_name}`.toLowerCase() ??
+						"unknown",
 				};
 			},
 		}),

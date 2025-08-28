020c094e05e9d837897b0218f0685066dee05c37
Brayan Monge Méndez <bmongemendez@gmail.com>
2025-08-27 21:47:23 -0600

    update import

--- src/components/CreatePackageListForm.tsx
+++ src/components/CreatePackageListForm.tsx
@@ -6,7 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { CheckIcon, Edit } from "lucide-react/icons";
+import { CheckIcon, Edit } from "lucide-react";
 import {
 	Dialog,
 	DialogContent,
